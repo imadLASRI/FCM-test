5e419a18a5df000cdb02cfb3b088e60b9c5fe230
LASRI Imad <imadlasri@hotmail.fr>
2020-07-14 09:10:31 +0000

    initial

---
+++ init-firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBnNSx9IyPZVwtEOcD6lLPclldpsTOQpao",
+  authDomain: "fcmtest-8284d.firebaseapp.com",
+  databaseURL: "https://fcmtest-8284d.firebaseio.com",
+  projectId: "fcmtest-8284d",
+  storageBucket: "fcmtest-8284d.appspot.com",
+  messagingSenderId: "500464585581",
+  appId: "1:500464585581:web:c9b55c426f8b2293232864",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
